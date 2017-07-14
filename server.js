4174b6b838973192afc0228f1d66d58127ecc8bc
Josep <josep.arpi@gmail.com>
2017-07-14 22:59:19 +0200

    Features Add features for server side - Load styles dynamically - Generate output format dynamycally - Rollback to original map size in index.html

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const port = 8888;
 
 mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins, 'shape.input'));
 
-// const TILE_SIZE = 4096;
-
 const TILE_SIZE = 256;
 
 app.get('/', function(req, res){
@@ -21,37 +19,27 @@ app.get('/', function(req, res){
 
 app.get('/:style/:z/:x/:y', function(req, res) {
     const map = new mapnik.Map(TILE_SIZE, TILE_SIZE);
-    map.load('./style-admin0.xml', function(err, map) {
-        // console.dir(map.__proto__);
-        // console.dir(map);
-        // map.zoomAll();
-        // map.extent = [310746, 5048810, 1214565, 5753452];
+    map.load(`./style-${req.params.style}.xml`, function(err, map) {
         console.dir(req.params);
         const z = req.params.z;
         const x = req.params.x;
         const y = req.params.y.substr(0, req.params.y.indexOf('.'));
-        const format = req.params.y.substr(req.params.y.indexOf('.'));
+        const format = req.params.y.substr(req.params.y.indexOf('.')+1);
         console.dir(`Z: ${z} X: ${x} Y: ${y} format: ${format}`);
         const square = mercator.bbox(x, y, z, false, '900913');
         map.extent = square;
-        console.dir(map);
         const im = new mapnik.Image(TILE_SIZE, TILE_SIZE);
         map.render(im, function(err, im) {
-            im.encode('png', function(err, buffer) {
-                // fs.writeFile('map.png', buffer);
+            im.encode(format, function(err, buffer) {
                 res.write(buffer);
                 res.end();
              });
         });
      });
-
-    console.log('Successful get');
-    // res.end('OK!');
 });
 
 app.listen(port, function(err){
     if (err) console.dir('Error initializing server');
     console.log('Server listening at ' + port);
 });
-
 
