69b983231df0ec32618c1b47b51c20a55a354d6d
Josep <josep.arpi@gmail.com>
2017-07-15 16:47:39 +0200

    Feature refactor Major changes in app init, routes init - Move map resources to assets folder - Add layer to register endpoints - Add controller layer to refactor core functionalities - Use gulp to minify resources for client side - Promisfy functions

--- server.js
+++ server.js
@@ -1,48 +1,34 @@
 'use strict';
 
 const fs = require('fs');
-const mapnik = require('mapnik');
-const path = require('path');
-const SphericalMercator = require('sphericalmercator');
-const mercator = new SphericalMercator({ size:256 });
 const express = require('express');
 const app = express();
 const port = 8888;
+const routes = require('./lib/routes/index.js');
 
-app.use('/public', express.static(__dirname + '/public'));
+const PUBLIC_DIR = '/public';
 
-mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins, 'shape.input'));
+app.use('/public', express.static(__dirname + PUBLIC_DIR));
 
-const TILE_SIZE = 256;
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/:style/:z/:x/:y', function(req, res) {
-    const map = new mapnik.Map(TILE_SIZE, TILE_SIZE);
-    map.load(`./style-${req.params.style}.xml`, function(err, map) {
-        console.dir(req.params);
-        const z = req.params.z;
-        const x = req.params.x;
-        const y = req.params.y.substr(0, req.params.y.indexOf('.'));
-        const format = req.params.y.substr(req.params.y.indexOf('.')+1);
-        console.dir(`Z: ${z} X: ${x} Y: ${y} format: ${format}`);
-        const square = mercator.bbox(x, y, z, false, '900913');
-        console.dir(map);
-        map.extent = square;
-        const im = new mapnik.Image(TILE_SIZE, TILE_SIZE);
-        map.render(im, function(err, im) {
-            im.encode(format, function(err, buffer) {
-                res.write(buffer);
-                res.end();
-             });
+function initHttp() {
+    return new Promise(function(resolve, reject) {
+        app.listen(port, function(err) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(true);
         });
-     });
-});
+    });
+}
 
-app.listen(port, function(err){
-    if (err) console.dir('Error initializing server');
-    console.log('Server listening at ' + port);
-});
+initHttp()
+    .then(function() {
+        console.log('app initialized on port ' + port);
+    })
+    .then(function() {
+        return routes(app);
+    })
+    .catch(function(err) {
+        console.dir(err);
+    });
 
