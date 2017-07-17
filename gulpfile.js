3d14da7b1b9205c480317ca69be3264ff515b60e
Josep <josep.arpi@gmail.com>
2017-07-17 22:30:11 +0200

    Feature / Bugfix Refactor code, tests, linting - Return proper errors (Promise.reject(new Error...)) - Handle promise rejections as 400 HTTP errors (it comes from param validation mainly) - Add lint tool (standard code style) - Add tasks into package.json and gulp - Add tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-const gutil = require('gulp-util');
-const livereload = require('gulp-livereload');
-const watch = require('gulp-watch');
+const path = require('path')
+const gulp = require('gulp')
+const mocha = require('gulp-mocha')
+const standard = require('gulp-standard')
 
-const plugins = require("gulp-load-plugins")({
-    pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'print'],
-	replaceString: /\bgulp[\-.]/
-});
+const plugins = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'print'],
+  replaceString: /\bgulp[-.]/
+})
 
-const dest = __dirname + '/public/';
+const dest = path.join(__dirname, '/public/')
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
+  return gulp.src(plugins.mainBowerFiles(), { base: './bower_components' })
+        .pipe(plugins.filter('**/*.js'))
+        .pipe(plugins.concat('main.js'))
+        .pipe(gulp.dest(dest + '/js'))
+})
 
-	gulp.src(plugins.mainBowerFiles(), { base: './bower_components' })
-		.pipe(plugins.filter('**/*.js'))
-		.pipe(plugins.concat('main.js'))
-		.pipe(gulp.dest(dest + '/js'));
+gulp.task('css', function () {
+  return gulp.src(plugins.mainBowerFiles(), { base: './bower_components' })
+        .pipe(plugins.filter('**/*.css'))
+        .pipe(plugins.concat('normalize.css'))
+        .pipe(gulp.dest(dest + '/css'))
+})
 
-});
+gulp.task('img', function () {
+  return gulp.src(plugins.mainBowerFiles(), { base: './bower_components' })
+        .pipe(plugins.filter('**/*.png'))
+        .pipe(plugins.print())
+        .pipe(plugins.flatten())
+        .pipe(gulp.dest(dest + '/css/images'))
+})
 
-gulp.task('css', function() {
+gulp.task('test', function () {
+  return gulp.src(['spec/**/*.js'], { read: false })
+    .pipe(mocha({ reporter: 'spec' }))
+})
 
-	gulp.src(plugins.mainBowerFiles(), { base: './bower_components' })
-		.pipe(plugins.filter('**/*.css'))
-		.pipe(plugins.concat('normalize.css'))
-		.pipe(gulp.dest(dest + '/css'));
+gulp.task('watch', function () {
+  return gulp.watch(['lib/**/*.js', 'spec/**/*.js'], ['scripts', 'css', 'img', 'lint', 'test'])
+})
 
-});
+gulp.task('lint', function () {
+  return gulp.src(['./server.js', './lib/**/*.js', './spec/**/*.js'])
+    .pipe(plugins.print())
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
+})
 
-
-gulp.task('img', function() {
-
-	gulp.src(plugins.mainBowerFiles(), { base: './bower_components' })
-		.pipe(plugins.filter('**/*.png'))
-		.pipe(plugins.print())
-		.pipe(plugins.flatten())
-		.pipe(gulp.dest(dest + '/css/images'));
-
-});
-
-gulp.task('test', function() {
-    gulp.src(['spec/**/*.js'], { read: false  })
-            .pipe(mocha({ reporter: 'spec'  }))
-});
-
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch(['lib/**/*.js', 'spec/**/*.js'], ['scripts', 'css', 'img', 'test']);
-});
-
-gulp.task('default', ['scripts', 'css', 'img', 'test', 'watch']);
-
+gulp.task('default', ['scripts', 'css', 'img', 'lint', 'test', 'watch'])
--- server.js
+++ server.js
@@ -1,35 +1,30 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const express = require('express');
-const app = express();
-const port = 8888;
-const routes = require('./lib/routes/index.js');
-
-const PUBLIC_DIR = '/public';
-
-app.use('/public', express.static(__dirname + PUBLIC_DIR));
+const express = require('express')
+const app = express()
+const port = 8888
+const routes = require('./lib/routes/index.js')
 
 const initHttp = () => {
-    return new Promise((resolve, reject) => {
-        app.listen(port, (err) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(true);
-        });
-    });
+  return new Promise((resolve, reject) => {
+    app.listen(port, (err) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(true)
+    })
+  })
 }
 
 initHttp()
     .then(() => {
-        console.log('app initialized on port ' + port);
+      console.log('app initialized on port ' + port)
     })
     .then(() => {
-        return routes(app);
+      return routes(app)
     })
     .catch((err) => {
-        console.dir(err);
-    });
+      console.dir(err)
+    })
 
-module.exports = app;
+module.exports = app
