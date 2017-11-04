8673c70c1caa7797f97e3e9539ec9d0c47ef59fd
Josep <josep.arpi@gmail.com>
2017-11-04 10:22:26 +0100

    Refactor Bugfixes / Codestyle issues - Init http when routes loaded - Get format from URL - True async code

--- server.js
+++ server.js
@@ -6,25 +6,23 @@ const port = 8888
 const routes = require('./lib/routes/index.js')
 
 const initHttp = () => {
-  return new Promise((resolve, reject) => {
-    app.listen(port, (err) => {
-      if (err) {
-        return reject(err)
-      }
-      return resolve(true)
-    })
-  })
+    return routes(app)
+        .then(_ => {
+            app.listen(port, (err) => {
+                if (err) {
+                    throw (err)
+                }
+                return true
+            })
+        })
 }
 
 initHttp()
     .then(() => {
-      console.log('app initialized on port ' + port)
-    })
-    .then(() => {
-      return routes(app)
+        console.log('app initialized on port ' + port)
     })
     .catch((err) => {
-      console.dir(err)
+        console.dir(err)
     })
 
 module.exports = app
